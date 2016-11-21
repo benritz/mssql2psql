926a761934754562097b2efa59cda129d7de00d6
Ben Ritz <ben.c.ritz@gmail.com>
2016-11-21 10:29:26 +0000

    added table data added command line options

--- mssql2psql.js
+++ mssql2psql.js
@@ -1,72 +1,202 @@
 "use strict"
 
-var sql = require('mssql');
+function writeTable(table, columns) {
+	var f = function(resolve, reject) {
+		// get columns def
+		var columnDefs = "";
 
-var out = process.stdout;
+		for (let column of columns) {
+			if (columnDefs)
+				columnDefs += ",\n";
 
-/**
- * Generate table defs.
- */
-function generateTableDefs() {
-    return sql.query`select t.name as table_name, c.column_id, c.name as column_name, c.max_length, c.precision, c.scale, c.is_nullable, c.is_identity, ty.name as type from sys.tables t, sys.columns c, sys.types ty where t.object_id = c.object_id and c.user_type_id = ty.user_type_id order by t.name asc, t.object_id asc, c.column_id asc`.then(function(rows) {
-    		var table, columns;
+			columnDefs += column.column_name + " ";
 
-			var writeDef = function() { out.write(`drop table if exists ${table};\n\ncreate table ${table}\n(\n${columns}\n);\n\n`); };
-
-			if (rows.length) {
-				for (let row of rows) {
-					if (row.column_id === 1) {
-						if (table) {
-							writeDef.call();
-						}
-
-						table = row.table_name;
-						columns = "";
+			if (column.is_identity) {
+				columnDefs += "serial";
+			} else if (column.type === "varchar" || column.type === "nvarchar" || 
+						column.type === "text" || column.type === "ntext" ||
+						(column.type === "char" && column.max_length > 1)) {
+				if (options.forceCaseInsensitive) {
+					columnDefs += "citext";
+				} else {
+					if (column.max_length === -1) {
+						columnDefs += "text";
 					} else {
-						columns += ",\n";
-					}
-				
-					columns += row.column_name + " ";
-
-					if (row.is_identity) {
-						columns += "serial";
-					} else if (row.type === "varchar" || row.type === "nvarchar") {
-						if (row.max_length === -1) {
-							columns += "text";
-						} else {
-							var maxLen = row.max_length;
-							if (row.type === "nvarchar") {
-								maxLen /= 2;
-							}
-							columns += "varchar(" + (maxLen) + ")";
+						var maxLen = column.max_length;
+						if (column.type === "nvarchar") {
+							maxLen /= 2;
 						}
-					} else if (row.type === "datetime") {
-						columns += "timestamp";
-					} else if (row.type === "image") {
-						columns += "bytea";
-					} else if (row.type === "bit") {
-						columns += "boolean";
-					} else {
-						columns += row.type;
+						columnDefs += "varchar(" + (maxLen) + ")";
 					}
-																																																																										  
-					if (!row.is_nullable) {
-						columns += " not";
-					}
-					columns += " null";
 				}
-			
-				writeDef.call();
+			} else if (column.type === "datetime") {
+				columnDefs += "timestamp";
+			} else if (column.type === "image") {
+				columnDefs += "bytea";
+			} else if (column.type === "bit") {
+				columnDefs += "boolean";
+			} else {
+				columnDefs += column.type;
 			}
+																																																																							  
+			if (!column.is_nullable) {
+				columnDefs += " not";
+			}
+			columnDefs += " null";
+		}		
+
+		// create streaming recordset
+		// write table def when the recordset is opened and then write insert for each row
+		// although this is done in a promise the ms sql tedious driver blocks other 
+		// recordset streams until this one is finished so the output for a table is
+		// sequential
+		var request = new sql.Request();
+		request.stream = true;
+		
+		request.query(`select * from ${table}`);
+
+		request.on("recordset", function() {
+			// write table def
+			out.write(`/* -- ${table} -- */\ndrop table if exists ${table};\n\ncreate table ${table}\n(\n${columnDefs}\n);\n\n`);
+		});
+
+		var n = 0;
+
+		request.on('row', function(row) {
+			var values = "";
+
+			Object.keys(row).forEach(function(field) {
+				var value = row[field];
+
+				if (values) {
+					values += ", ";
+				}
+
+				if (typeof value === "string") {
+					values += "'" + value.replace("'", "''") + "'";
+				} else if (value instanceof Date) {
+					values += "'" + value.toISOString() + "'";
+				} else {
+					values += value;
+				}
+			});
+
+			if (options.dataBatchSize === 1) {
+				out.write(`insert into ${table} values (${values});\n`);
+			} else {
+				if (n % options.dataBatchSize === 0) {
+					if (n !== 0) {
+						out.write(";\n\n");
+					}
+					out.write(`insert into ${table} values (${values})`);
+				} else {
+					out.write(`\n,(${values})`);
+				}
+			}
+
+			++n;
+		});
+
+		request.on('error', function(err) {
+			reject(err);
+		});
+
+		request.on('done', function(affected) {
+			out.write(";\n");
+
+			if (n !== 0) {
+				out.write("\n");
+			}
+
+			resolve();
+		});
+	};
+
+	return new Promise(function(resolve, reject) { f(resolve, reject); });
+}
+
+function writeTables() {
+    return sql.query`select t.name as table_name, c.column_id, c.name as column_name, c.max_length, c.precision, c.scale, c.is_nullable, c.is_identity, ty.name as type from sys.tables t, sys.columns c, sys.types ty where t.object_id = c.object_id and c.user_type_id = ty.user_type_id order by t.name asc, t.object_id asc, c.column_id asc`.then(function(rows) {
+		out.write("/* --------------------- TABLES --------------------- */\n\n");
+
+		// enable case insensitive extension
+		out.write("-- enable case insensitive extension\n");
+		out.write("create extension citext;\n\n");
+
+		// write table DDL and data
+		var a = [];
+
+		if (rows.length) {
+			var table, columns;
+
+			for (let row of rows) {
+				if (row.column_id === 1) {
+					if (table) {
+						a.push(writeTable(table, columns));
+					}
+
+					table = row.table_name;
+					columns = [];
+				}
+
+				var column = row;
+				delete column.table_name;
+				columns.push(column);
+			}
+
+			a.push(writeTable(table, columns));
+		}
+
+		return Promise.all(a);
     }).catch(function(e) {
-         console.error(e);
-         });
+		console.error(e);
+	});
+}
+
+function writeSeqReset() {
+	// create function to get the maximum value for a sequence's field
+	// see http://stackoverflow.com/a/5943183/1095458
+	out.write("-- function to find a sequence's field's maximum value, this is used to set the sequence's next value after the data is inserted\n");
+	out.write("-- see http://stackoverflow.com/a/5943183/1095458");
+	out.write(`create or replace function seq_field_max_value(oid) returns bigint
+	volatile strict language plpgsql as  $$
+	declare
+	 tabrelid oid;
+	 colname name;
+	 r record;
+	 newmax bigint;
+	begin
+	 for tabrelid, colname in select attrelid, attname
+				   from pg_attribute
+				  where (attrelid, attnum) in (
+						  select adrelid::regclass,adnum
+							from pg_attrdef
+						   where oid in (select objid
+										   from pg_depend
+										  where refobjid = $1
+												and classid = 'pg_attrdef'::regclass
+										)
+			  ) loop
+		  for r in execute 'select max(' || quote_ident(colname) || ') from ' || tabrelid::regclass loop
+			  if newmax is null or r.max > newmax then
+				  newmax := r.max;
+			  end if;
+		  end loop;
+	  end loop;
+	  return newmax;
+	end; $$ ;\n\n`);
+	
+	// set any sequence to the maximum value of the sequence's field
+	out.write("-- set any sequence to the maximum value of the sequence's field\n");
+	out.write("select relname, setval(oid, seq_field_max_value(oid)) from pg_class where relkind = 'S';\n\n");
 }
 
 /**
- * Generate foreign key defs.
+ * Write foreign key defs.
  */
-function generateForeignKeyDefs() {
+function writeForeignKeyes() {
+	out.write("/* --------------------- FOREIGN KEYS --------------------- */\n\n");
+
 	return sql.query`select 
 fk.name as key_name, t.name as parent_table, c.name as parent_column, rt.name as referenced_table, rc.name as referenced_column, fkc.constraint_column_id as constraint_column_id
 from 
@@ -80,12 +210,16 @@ fk.object_id asc, fkc.constraint_column_id asc`.then(function(rows) {
 		if (rows.length) {
 			var key, parentColumns, parentTable, referencedColumns, referencedTable;
 
-			var writeDef = function() { out.write(`alter table ${parentTable} add constraint ${key} foreign key (${parentColumns}) references ${referencedTable} (${referencedColumns});\n`); };
+			var f = function() { out.write(`alter table ${parentTable} add constraint ${key} foreign key (${parentColumns}) references ${referencedTable} (${referencedColumns});\n`); };
 
 			for (let row of rows) {
 				if (row.constraint_column_id === 1) {
 					if (parentTable) {
-						writeDef.call();
+						f();
+
+						if (parentTable !== row.parent_table) {
+							out.write("\n");
+						}
 					}
 
 					key = row.key_name;
@@ -101,15 +235,19 @@ fk.object_id asc, fkc.constraint_column_id asc`.then(function(rows) {
 				referencedColumns += row.referenced_column;
 			}
 
-			writeDef.call();
+			f();
+
+			out.write("\n");
 		}
 	});
 }
 
 /**
- * Generate primary key, unique key and index defs.
+ * Write primary key, unique key and index defs.
  */
-function generateIndexDefs() {
+function writeIndexes() {
+	out.write("/* --------------------- PRIMARY KEYS, UNIQUE CONSTRAINTS AND INDEXES --------------------- */\n\n");
+
 	return sql.query`select 
 t.name as table_name, c.name as column_name, i.name as index_name, ic.index_column_id, i.is_primary_key, i.is_unique_constraint, i.is_unique 
 from 
@@ -121,7 +259,7 @@ order by t.name asc, t.object_id asc, i.index_id asc, ic.index_column_id asc`.th
 		if (rows.length) {
 			var indexName, table, columns, isPrimary, isUniqueConst, isUnique;
 
-			var writeDef = function() {
+			var f = function() {
 				if (isPrimary) {
 					out.write(`alter table ${table} add constraint ${indexName} primary key (${columns});\n`);
 				} else if (isUniqueConst) {
@@ -136,7 +274,11 @@ order by t.name asc, t.object_id asc, i.index_id asc, ic.index_column_id asc`.th
 			for (let row of rows) {
 				if (row.index_column_id === 1) {
 					if (table) {
-						writeDef.call();
+						f();
+
+						if (table !== row.table_name) {
+							out.write("\n");
+						}
 					}
 
 					table = row.table_name;
@@ -152,48 +294,97 @@ order by t.name asc, t.object_id asc, i.index_id asc, ic.index_column_id asc`.th
 				columns += row.column_name;
 			}
 			
-			writeDef.call();
+			f();
+
+			out.write("\n");
 		}
 	});
 }
 
 /**
- * Generate default defs.
+ * Write default defs.
  */
-function generateDefaultDefs() {
+function writeDefaults() {
+	out.write("/* --------------------- DEFAULTS --------------------- */\n\n");
+
 	return sql.query`select
-t.name as table_name, c.name as column_name, d.name as default_name, d.definition 
+t.name as table_name, c.name as column_name, ty.name as type, d.name as default_name, d.definition 
 from 
-sys.tables t, sys.columns c, sys.default_constraints d 
+sys.tables t, sys.columns c, sys.types ty, sys.default_constraints d 
 where 
-t.object_id = c.object_id and t.object_id = d.parent_object_id and c.column_id = d.parent_column_id
+t.object_id = c.object_id and c.user_type_id = ty.user_type_id and t.object_id = d.parent_object_id and c.column_id = d.parent_column_id
 order by t.name asc, t.object_id asc, c.column_id asc`.then(function(rows) {
 		if (rows.length) {
+			var table;
 			for (let row of rows) {
+				if (table && table !== row.table_name) {
+					out.write("\n");
+				}
+			
+				table = row.table_name;
 				var definition = row.definition;
 				
 				definition = definition.replace(/^\(\((\d+)\)\)$/, "$1");
 				definition = definition.replace("(getdate())", "now()");
+				
+				if (row.type === "bit") {
+					if (definition === "1") {
+						definition = "true";
+					} else if (definition === "0") {
+						definition = "false";
+					}
+				}
 
-				out.write(`alter table ${row.table_name} alter column ${row.column_name} set default ${definition};\n`);
+				out.write(`alter table ${table} alter column ${row.column_name} set default ${definition};\n`);
 			}
+			
+			out.write("\n");
 		}
 	});
 }
 
+var minimist = require('minimist');
 
-sql.connect("mssql://" + process.argv[2]).then(function() {
-	generateTableDefs().then(function() {
-		generateIndexDefs().then(function() {
-			generateForeignKeyDefs().then(function() {
-				generateDefaultDefs().then(function() {
-					process.exit(0);
-				});
-			});
-		});
-	});
-}).catch(function(e) {
+var options = minimist(process.argv.slice(2), { 
+	boolean: ["forceCaseInsensitive"], 
+	default: { "dataBatchSize": 100 } });
+var args = options._;
+delete options._;
+
+if (args.length === 0) {
+	console.error("Missing the connection URL.");
+	process.exit(1);
+}
+
+var url = args[0];
+
+var out;
+
+if (args.length === 1) {
+	out = process.stdout;
+} else {
+	var fs = require('fs');
+	out = fs.createWriteStream(args[1]);
+}
+
+var sql = require('mssql');
+
+sql.connect(url)
+	.then(writeTables)
+	.then(writeSeqReset)
+	.then(writeIndexes)
+	.then(writeForeignKeyes)
+	.then(writeDefaults)
+	.then(function() {
+		if (out !== process.stdout) {
+			out.end();
+		}
+		process.exit(0);
+	})
+	.catch(function(e) {
      console.error(e);
-
      process.exit(1);
 });
+
+
+// create view
